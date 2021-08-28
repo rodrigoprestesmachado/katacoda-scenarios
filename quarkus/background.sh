b6558f8d59fa0ff7a53ffc1079e71ce3dfbd2bfc
Rodrigo <rodrigo.prestes@gmail.dom>
2021-08-28 11:45:46 -0300

    test

--- quarkus/background.sh
+++ quarkus/background.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 echo "Installing environment"
-
 apt install default-jre
 apt install maven
 curl -Ls https://sh.jbang.dev | bash -s - app install --fresh --force quarkus@quarkusio
 jbang app install --fresh --force quarkus@quarkusio
-
+jbang app install --force --name qss ~/.m2/repository/io/quarkus/quarkus-cli/999-SNAPSHOT/quarkus-cli-999-SNAPSHOT-runner.jar
 echo "done" >> /opt/.backgroundfinished
