f9b2a71cabe83a7ca67912c8d3d443046cc672bc
Rodrigo <rodrigo.prestes@gmail.dom>
2021-10-12 18:53:09 -0300

    foreground

--- quarkus/foreground.sh
+++ quarkus/foreground.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-echo "Waiting to complete";
-while [ ! -f /opt/.backgroundfinished ] ;
-do sleep 2;
-done;
-echo "Done"
+echo "Waiting to complete"; while [ ! -f /opt/.backgroundfinished ] ; do sleep 2; done; echo "Done"
